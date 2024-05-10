3c68d030d42df1d44589e54fd95ae1d745e7f1ae
Valentin Bouiller <31282661+vbouiller@users.noreply.github.com>
2024-05-10 19:20:47 +0200

    fix: format variables

--- variables.tf
+++ variables.tf
@@ -2,32 +2,32 @@
 
 variable "hvn_cloud_provider" {
   description = "HVN cloud provider, defaults to 'aws'"
-  type = string
-  default = "aws"
+  type        = string
+  default     = "aws"
 }
 
 variable "hvn_region" {
   description = "HVN region, defaults to eu-central-1"
-  type = string
-  default = "eu-central-1"
+  type        = string
+  default     = "eu-central-1"
 }
 
 variable "hvn_id" {
   description = "HVN name"
-  type = string
-  default = "platform-hvn"
+  type        = string
+  default     = "platform-hvn"
 }
 
 # VAULT variables
 
 variable "vault_id" {
-    description = "Vault cluster name"
-    type = string
-    default = "platform-vault"
+  description = "Vault cluster name"
+  type        = string
+  default     = "platform-vault"
 }
 
 variable "vault_tier" {
   description = "Tier of the HCP Vault cluster"
-  type = string
-  default = "plus_small"
+  type        = string
+  default     = "plus_small"
 }
