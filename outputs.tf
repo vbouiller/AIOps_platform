c228aafd0a73d49b8910a13a8ff2d36ce3286c0e
Valentin Bouiller <31282661+vbouiller@users.noreply.github.com>
2024-05-13 19:48:02 +0200

    fix: debug token

--- outputs.tf
+++ outputs.tf
@@ -3,28 +3,10 @@ output "vault_admin_token" {
   sensitive = true
 }
 
-output "org_id" {
-  value = data.hcp_organization.hcp_org.resource_id
-}
-
-output "proj_id" {
-  value = data.hcp_project.hcp_project.resource_id
-}
-
 output "answer" {
   value = data.http.hvs_apps.response_body
 }
 
-output "url" {
-  value     = data.http.hcp_api_token.url
-  sensitive = true
-}
-
-output "token_response" {
-  value = data.http.hcp_api_token.response_body
-}
-
-output "request" {
-  value     = data.http.hcp_api_token.request_body
-  sensitive = true
+output "token" {
+    value = jsondecode(data.http.hcp_api_token.response_body).access_token
 }
