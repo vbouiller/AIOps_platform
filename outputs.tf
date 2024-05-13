3a1ffb82148d013326e117d4a6a26a1ca117700c
Valentin Bouiller <31282661+vbouiller@users.noreply.github.com>
2024-05-13 17:06:59 +0200

    fix: added outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,15 @@ output "vault_admin_token" {
   value     = hcp_vault_cluster_admin_token.admin.token
   sensitive = true
 }
+
+output "org_id" {
+  value = data.hcp_organization.org.resource_id
+}
+
+output "proj_id" {
+  value = data.hcp_project.project.resource_id
+}
+
+output "answer" {
+  value = data.http.hvs_apps.response_body
+}
