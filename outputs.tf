4776164f75d97729bcef7f01949e7b2cabdb689c
Valentin Bouiller <31282661+vbouiller@users.noreply.github.com>
2024-05-21 18:31:50 +0200

    feat: added address and namespace output for Vault cluster

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,14 @@ output "vault_admin_token" {
   sensitive = true
 }
 
+output "vault_cluster_adress" {
+  value = hcp_vault_cluster.vault.public_endpoint
+}
+
+output "vault_cluster_namespace" {
+  value = hcp_vault_cluster.vault.namespace
+}
+
 output "answer" {
   value = jsondecode(data.http.get_hvs_apps.response_body).apps[*].name
 }
