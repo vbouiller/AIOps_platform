40d9a124fb0a2e5de25f45fc0bd7a38d6c7e3fd5
Valentin Bouiller <31282661+vbouiller@users.noreply.github.com>
2024-06-03 17:22:54 +0200

    fix: added allowed user to role

--- main.tf
+++ main.tf
@@ -61,6 +61,7 @@ resource "vault_ssh_secret_backend_role" "ca-signer" {
   backend                 = vault_mount.ssh.path
   key_type                = "ca"
   allow_user_certificates = true
+  allowed_users           = "*"
 }
 
 ## Vault Policies
